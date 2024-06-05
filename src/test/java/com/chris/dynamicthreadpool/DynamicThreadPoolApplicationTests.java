c46852174c63a2f34f60539ff8dbb2ee20ca2534
heshukun <chrisqingshan@163.com>
2024-06-05 11:03:19 +0800

    feat:搭建动态线程池

---
+++ src/test/java/com/chris/dynamicthreadpool/DynamicThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.dynamicthreadpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicThreadPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
